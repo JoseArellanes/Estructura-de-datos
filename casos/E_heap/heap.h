8722a37d33895bb475d304ff95e0b705843b059e
JoseArellanes <69484453+JoseArellanes@users.noreply.github.com>
2020-11-08 21:17:46 -0600

    D_hash completo

--- casos/E_heap/heap.h
+++ casos/E_heap/heap.h
@@ -19,13 +19,13 @@ private:
 	T *data;
 	unsigned int size;
 	unsigned int count;
-	
+
 	unsigned int parent(unsigned int) const;
 	unsigned int left(unsigned int) const;
 	unsigned int right(unsigned int) const;
 	void heapify(unsigned int);
 	void swap(unsigned int, unsigned int);
-	
+
 public:
 	Heap(unsigned int) throw (OutOfMemory);
 	~Heap();
@@ -39,34 +39,51 @@ public:
 
 template <class T>
 Heap<T>::Heap(unsigned int sze) throw (OutOfMemory) {
+  size = sze;
+  count = 0;
+  data = new T[size];
+  if (data == NULL) {
+    throw OutOfMemory();
+  }
 }
 
 template <class T>
 Heap<T>::~Heap() {
+  delete []data;
+  data = NULL;
+  size = 0;
+  count = 0;
 }
 
 template <class T>
 bool Heap<T>::empty() const {
+  if (count == 0) {
+    return true;
+  }
+  return false;
 }
 
 template <class T>
 bool Heap<T>::full() const {
+  if (count == size) {
+    return true;
+  }
 	return false;
 }
 
 template <class T>
 unsigned int Heap<T>::parent(unsigned int pos) const {
-	return 0;
+	return (pos - 1) / 2;
 }
 
 template <class T>
 unsigned int Heap<T>::left(unsigned int pos) const {
-	return 0;
+	return (2 * pos) + 1;
 }
 
 template <class T>
 unsigned int Heap<T>::right(unsigned int pos) const {
-	return 0;
+	return (2 * pos) + 2;
 }
 
 template <class T>
@@ -82,24 +99,44 @@ void Heap<T>::heapify(unsigned int pos) {
 
 template <class T>
 void Heap<T>::add(T val) throw (Overflow) {
+
+  //Verificar que no esté lleno el heap
+  if(full()) {
+    throw Overflow();
+  }
+
+  //Obtener la siguiente posición disponible
+  int posicion = count;
+  //Agregar el nuevo valor en la siguiente posición
+  data[posicion] = val;
+  //Aumentar el count
+  count++;
+
+  //reheap up
+  //si el padre es más grande, hacer swap
+  std::cout << posicion << ": " << data[posicion]<< std::endl;
+  if (posicion > 0 && data[parent(posicion)] > val) {
+    swap(data[posicion], data[parent(posicion)]);
+  }
+
 }
 
 template <class T>
 T Heap<T>::remove() throw (NoSuchElement) {
 	T aux;
-	
+
 	return aux;
 }
 
 template <class T>
 void Heap<T>::clear() {
 }
-	
+
 
 template <class T>
 std::string Heap<T>::toString() const {
 	std::stringstream aux;
-	
+
 	aux << "[";
 	for (unsigned int i = 0; i < count; i++) {
 		if (i != 0) {
@@ -111,4 +148,4 @@ std::string Heap<T>::toString() const {
 	return aux.str();
 }
 #endif /* HASH_H_ */
-	
+
